bcb4b42948f1bbba673995c3099f42868cfb3c37
Richard Iannone <riannone@me.com>
2021-02-04 01:40:55 -0500

    Update _common.R

--- book/_common.R
+++ book/_common.R
@@ -1,4 +1,4 @@
-set.seed(1014)
+set.seed(23)
 
 knitr::opts_chunk$set(
   comment = "#>",
@@ -13,5 +13,5 @@ knitr::opts_chunk$set(
 
 options(dplyr.print_min = 6, dplyr.print_max = 6)
 
-# Supress crayon output
+# Suppress crayon output
 options(crayon.enabled = FALSE)
