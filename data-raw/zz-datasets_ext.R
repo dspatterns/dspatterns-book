597a82463b9cbfa6939c1ed69643032aaf7e2087
Richard Iannone <riannone@me.com>
2021-03-19 16:45:34 -0400

    Update zz-datasets_ext.R

--- data-raw/zz-datasets_ext.R
+++ data-raw/zz-datasets_ext.R
@@ -3,33 +3,33 @@ library(usethis)
 # This script uses `usethis::use_data()` to add data to the package
 
 # Source all of the datasets
-source("data-raw/02-sw.R")
-source("data-raw/03-dmd.R")
-source("data-raw/04-winniweather.R")
-source("data-raw/05-us_cities.R")
-source("data-raw/06-nycweather.R")
-source("data-raw/07-sales.R")
-source("data-raw/08-resto_reviews.R")
-source("data-raw/09-german_cities.R")
-source("data-raw/10a-employment.R")
-source("data-raw/10b-rainfall.R")
-source("data-raw/10c-imdb.R")
-source("data-raw/10d-pitchfork.R")
+source("data-raw/sw.R")
+source("data-raw/dmd.R")
+source("data-raw/winniweather.R")
+source("data-raw/us_cities.R")
+source("data-raw/nycweather.R")
+source("data-raw/sales.R")
+source("data-raw/resto_reviews.R")
+source("data-raw/german_cities.R")
+source("data-raw/employment.R")
+source("data-raw/rainfall.R")
+source("data-raw/imdb.R")
+source("data-raw/pitchfork.R")
 
 # Create external datasets
 use_data(
-  sw,                   # 02
-  dmd,                  # 03
-  winniweather,         # 04
-  us_cities,            # 05
-  nycweather,           # 06
-  sales,                # 07
-  resto_reviews,        # 08
-  german_cities,        # 09
-  employment,           # 10a
-  rainfall,             # 10b
-  imdb,                 # 10c
-  pitchfork,            # 10d
+  sw,
+  dmd,
+  winniweather,
+  us_cities,
+  nycweather,
+  sales,
+  resto_reviews,
+  german_cities,
+  employment,
+  rainfall,
+  imdb,
+  pitchfork,
   internal = FALSE,
   overwrite = TRUE
 )
