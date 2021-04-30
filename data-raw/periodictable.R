782d29247676d560068b3e8f2d28055e1608bbbc
Richard Iannone <riannone@me.com>
2021-04-30 11:03:00 -0400

    Create periodictable.R

---
+++ data-raw/periodictable.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(jsonlite)
+
+#
+# The `periodic_table` dataset
+#
+
+periodic_table <-
+  jsonlite::fromJSON(
+    "data-raw/periodictable.json",
+    ) %>%
+  dplyr::as_tibble() %>%
+  tidyr::unpack(cols = everything()) %>%
+  dplyr::select(
+    number, symbol, name, atomic_mass, category,
+    phase, boil, melt, molar_heat, density,
+    ea = electron_affinity,
+    en = electronegativity_pauling,
+    appearance, period, xpos, ypos,
+    discovered_by
+  )
